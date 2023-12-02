4323613185d8eb9232d40d6603412614535bc2f8
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-12-02 11:35:34 +0100

    Init

---
+++ src/test/java/cphbusiness/noinputs/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cphbusiness.noinputs.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
